ecbaf52ff48503f23db02b18333ef128bf2d18a3
Rakibul Yeasin <rytotul@gmail.com>
2020-03-24 00:59:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
     data = request.args.to_dict()
-    if request.args.get('last clicked button name') == 'Get Started' and request.args.get('last user freeform input') == '':
+    if request.args.get('last clicked button name') == 'Get Started' or request.args.get('last user freeform input') == '':
         message = '/start'
     else:
         message = request.args.get('last user freeform input')
